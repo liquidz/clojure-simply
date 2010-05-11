6ec0b6ddd6d32fd9343d0c34a012a98497518bf2
masa <masa@uopc.(none)>
2010-05-11 22:31:06 +0900

    update

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject
-  org.clojars.liquidz/simply "0.0.6"
+  org.clojars.liquidz/simply "0.0.7"
   :description "write clojure code more simply"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]

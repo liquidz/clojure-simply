382e486aa25782bd9dbc0a6d070181523b80e665
liquidz <liquidz.uo@gmail.com>
2010-05-17 16:56:14 +0900

    update defnk

--- src/simply.clj
+++ src/simply.clj
@@ -52,16 +52,29 @@
   )
 
 ; =defnk
+(defn split-kv-extra [col]
+  (loop [ls col, m {}]
+    (cond
+      (empty? ls) [m ()]
+      (-> ls first keyword?) (recur (cddr ls) (assoc m (first ls) (second ls)))
+      :else [m ls]
+      )
+    )
+  )
 (defmacro fnk [args & body]
   (let [[fixed-args k-args] (split-with symbol? args)
-        s-args (map #(if (keyword? %) (keyword->symbol %) %) k-args)
+        [k-args2 extra] (split-with #(not (= % '&)) k-args)
+        s-args (map #(if (keyword? %) (keyword->symbol %) %) k-args2)
+        extra-name (if (empty? extra) '_extra_ (-> extra second symbol))
         keywords (filter symbol? s-args)
         default-map (apply hash-map s-args)
         [condition-map & rest-body] (if (and (rest body) (map? (first body))) body (cons () body))
         ]
     `(fn [~@fixed-args & more#]
        ~condition-map
-       (let [{:keys [~@keywords] :or ~default-map} (apply hash-map more#)]
+       (let [[kvs# ~extra-name] (split-kv-extra more#)
+             {:keys [~@keywords] :or ~default-map} kvs#
+             ]
          ~@rest-body
          )
        )

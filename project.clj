495c3885b11ef561a2ccb5e2dae54abd5be5ac63
liquidz <liquidz.uo@gmail.com>
2010-05-18 18:59:38 +0900

    update

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject
-  org.clojars.liquidz/simply "0.0.8"
+  org.clojars.liquidz/simply "0.0.9"
   :description "write clojure code more simply"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]

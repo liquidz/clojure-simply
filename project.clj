a5ae0747193df7c78b7001a9b742270421a9fd0f
liquidz <liquidz.uo@gmail.com>
2010-05-20 14:14:16 +0900

    add lein-clojars

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  ]
+  :dev-dependencies [[lein-clojars "0.5-SNAPSHOT"]]
   :namespace [simply]
   :repositories {"clojars" "http://clojars.org/repo"}
   )

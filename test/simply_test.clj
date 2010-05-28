331a9d9698cf94182de4bdebe3966de26278e963
liquidz <liquidz.uo@gmail.com>
2010-05-28 18:57:27 +0900

    update

--- test/simply_test.clj
+++ test/simply_test.clj
@@ -134,6 +134,7 @@
   (is (nil? (foreach inc '(1 2) '(3 4))))
   (is (nil? (foreach inc [1 2 3])))
   (is (nil? (foreach (fn [[k v]] v) {:a 1 :b 2})))
+  (is (nil? (foreach (fn [[k v]] v) ())))
   (is (thrown? java.lang.AssertionError (foreach inc "hello")))
   (is (thrown? java.lang.AssertionError (foreach inc '(1 2) "hello")))
   (is (thrown? java.lang.AssertionError (foreach inc '(1 2) '(3 4) '5)))
